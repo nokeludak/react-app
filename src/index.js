45c5487aea3fd421048bcf74375dd58e4ca700ca
nokeludak <ksmajic996@gmail.com>
2021-05-02 18:13:08 +0200

    Proba

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import './index.css';
+import './assets/styles/site.css';
+import './assets/styles/table.css';
+import { render } from 'react-dom';
+import App from './App';
+
+
+
+render(<App />, document.getElementById('root'));
